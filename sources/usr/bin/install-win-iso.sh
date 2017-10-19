f37772b2d404708c09a6fee727e8ff2d6caa5b44
Denis Bonnenfant <denis.bonnenfant@diderot.org>
2017-10-19 21:51:34 +0200

    Le bon script pour installer l iso

--- sources/usr/bin/install-win-iso.sh
+++ sources/usr/bin/install-win-iso.sh
@@ -2,14 +2,16 @@
 # script d'installation d'une iso Windows pour sambaedu-client-windows
 
 iso="$1"
-[ -z "$1" ] && echo "vous devez donner le nom de du fichier iso dans os/iso" && exit 1
-mkdir /mnt/iso
-mount -o loop "/var/se3/unattended/install/os/iso/$1" /mnt/iso
-cp -R /mnt/iso/* 
-chown -R adminse3:admins /var/se3/unattended/install/os/Win10
+[ -z "$iso" ] && echo "vous devez donner le nom de du fichier iso dans os/iso" && exit 1
+[ -f /mnt/iso ] || mkdir /mnt/iso
+mount -o loop  "/var/se3/unattended/install/os/iso/$iso" /mnt/iso
+cp -R /mnt/iso/* /var/se3/unattended/install/os/Win10/
+#chown -R adminse3:admins /var/se3/unattended/install/os/Win10
 
 echo "Sources Windows installées. Vous devez maintenant injecter les drivers réseau
 dans l'image de boot avec DISM depuis un poste windows 10" 
 umount /mnt/iso
+rm -fr /mnt/iso
+
 
 
