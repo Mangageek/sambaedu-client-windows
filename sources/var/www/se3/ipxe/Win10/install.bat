a48993e2997286d79ed301b3a83ed4332ceed1e2
denis-b <denis.bonnenfant@diderot.org>
2018-01-15 21:09:31 +0100

    Temporisation cracra pour etablir le reseau en winpe

--- sources/var/www/se3/ipxe/Win10/install.bat
+++ sources/var/www/se3/ipxe/Win10/install.bat
@@ -1,5 +1,11 @@
 wpeinit.exe  
 call se3w10-vars.cmd
+IPCONFIG /RENEW
+PING 127.0.0.1
+IPCONFIG /RENEW
+PING 127.0.0.1
+IPCONFIG /RENEW
+PING 127.0.0.1
 net use z: \\%SE3IP%\install /user:adminse3 %XPPASS%
 rem
 z:\os\Win10\setup.exe /unattend:z:\os\netinst\unattend.xml
